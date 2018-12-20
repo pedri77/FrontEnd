9a3906d1e25baa21fce4674f8036839874fbb5d8
pedri77 <dmgmoya85@gmail.com>
2018-12-20 12:13:56 +0100

    webpack-index

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
     output: {
     filename: 'bundle[hash].js',
     path: path.resolve(__dirname, 'dist') 
-  }, 
+    }, 
+    devServer: {
+    open: true,
+    overlay: true,
+    port: 3000,
+    hot: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+            title: 'Front End Avanzado PLAY',
+            template: path.join(__dirname, 'src', 'index')
+        }
+        )
+    ]
 };
